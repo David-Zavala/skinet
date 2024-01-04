b90b8d94cd441d762eec6edd26675d4fa8ddac9d
David Zavala <david.zavalal@udem.edu>
2024-01-03 22:33:17 -0600

    End of Section 2

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,10 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext(DbContextOptions<StoreContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
