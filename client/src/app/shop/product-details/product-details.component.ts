cf44fcf7c7f7323be2de1e55cffd7e932ab55152
David Zavala <david.zavalal@udem.edu>
2024-01-28 22:08:19 -0600

    End of Section 12 (Header, Breadcrumbs & Styling)

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -11,14 +12,25 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit{
   product: Product | undefined;
 
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private shopService: ShopService,
+    private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService
+  ) {
+    this.bcService.set('@productDetails', 'Loading...');
+  }
 
   ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if (id) {
       this.shopService.getProduct(+id).subscribe({
         next: product => {
           this.product = product;
+          this.bcService.set('@productDetails', product.name);
         },
         error: error => console.log(error)
 
